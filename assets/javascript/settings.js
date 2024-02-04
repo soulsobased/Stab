2b4cda768f162e56f98c33b1176047e98ccbdc11
soul <136810334+soulsobased@users.noreply.github.com>
2024-02-04 02:20:28 -0500

    Update settings.js

--- assets/javascript/settings.js
+++ assets/javascript/settings.js
@@ -2,7 +2,7 @@ class settings_struct {
   videoElement = null;
   audioElement = null;
   musicVolume = 0.9;
-  description_Texts = ['<3 Iire'];
+  description_Texts = ['.zay , 0z'];
 }
 
 const settings = new settings_struct();
