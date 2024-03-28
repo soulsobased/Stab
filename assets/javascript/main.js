6d3b53e10050ced29c29a00cee98a67af01b9a49
soul <136810334+soulsobased@users.noreply.github.com>
2024-03-27 22:45:59 -0400

    improve

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,76 +4,74 @@ const timeouts = [];
 
 const mobileAndTabletCheck = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-
-if ($.cookie('videoTime')) {
-  settings.videoElement.currentTime = $.cookie('videoTime');
-  settings.audioElement.currentTime = $.cookie('videoTime');
-}
-
-$('.start-btn').click(() => {
-  loadMain();
-});
-
-$.fn.extend({
-  animateCss: function (animationName) {
-    const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-    this.addClass(`animated ${animationName}`).one(animationEnd, () => {
-      $(this).removeClass(`animated ${animationName}`);
-    });
-
-    return this;
-  },
-});
-
+// Load main function
 const loadMain = () => {
-
+  // Clear timeouts
   timeouts.forEach((timeout) => {
     clearTimeout(timeout);
   });
 
+  // Remove start block
   $('.start-block').remove();
 
+  // Hide main and fade in container
   $('#main').fadeOut(100, () => {
     $('#main').remove();
 
+    requestAnimationFrame(() => {
+      // Pulse title block
+      $('.title_block').addClass('animated pulse');
+    });
 
-    setTimeout(() => {
-      $('.title_block').animateCss('pulse');
-    }, 200);
+    requestAnimationFrame(() => {
+      // Typed animation
+      if (settings.description_Texts) {
+        const typed = new Typed('#description_block', {
+          strings: settings.description_Texts,
+          typeSpeed: 40,
+          onComplete: () => {
+            clearCursor();
+          },
+        });
+      }
+    });
 
-    setTimeout(() => {
-      const typed = new Typed('#description_block', {
-        strings: settings.description_Texts,
-        typeSpeed: 40,
+    requestAnimationFrame(() => {
+      // Play video and audio
+      if (settings.videoElement && settings.audioElement) {
+        settings.videoElement.play();
+        settings.audioElement.play();
 
-        onComplete: () => {
-          clearCursor();
-        },
-      });
-    }, 1350);
+        settings.videoElement.addEventListener(
+          'timeupdate',
+          () => {
+            if ($.cookie) {
+              $.cookie('videoTime', settings.videoElement.currentTime, { expires: 1 });
+            }
+          },
+          false
+        );
 
-    setTimeout(() => {
-      settings.videoElement.play();
-      settings.audioElement.play();
+        $('.container').fadeIn();
 
-      settings.videoElement.addEventListener(
-        'timeupdate',
-        () => {
-          $.cookie('videoTime', settings.videoElement.currentTime, { expires: 1 });
-        },
-        false
-      );
-
-      $('.container').fadeIn();
-
-      $('.background').fadeIn(200, () => {
-        $('#audio').animate({ volume: settings.musicVolume }, 0);
-      });
-    }, 200);
+        requestAnimationFrame(() => {
+          $('.background').fadeIn(200, () => {
+            if (settings.musicVolume) {
+              $('#audio').animate({ volume: settings.musicVolume }, 0);
+            }
+          });
+        });
+      }
+    });
   });
 };
 
+// Clear cursor function
 const clearCursor = () => {
   return $('span').siblings('.typed-cursor').css('opacity', '0');
 };
+
+// Animate CSS function
+$.fn.extend({
+  animateCss: function (animationName) {
+    const
